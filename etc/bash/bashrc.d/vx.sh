42a5f7a4284ce261b0b19e765b5ceca9afcbf6a7
vxlinux <jason@customweb.co.za>
2022-02-14 14:41:51 +0200

    1.0b

---
+++ etc/bash/bashrc.d/vx.sh
@@ -0,0 +1,8 @@
+alias ls='ls --color=auto'
+alias install='sudo xbps-install -Su'
+alias query='sudo xbps-query -Rs'
+alias remove='sudo xbps-remove -Ro'
+alias services='sudo /usr/share/VX/modules/utility/services'
+alias update='sudo xbps-install -Su'
+alias vxinstaller='void-installer'
+NO_AT_BRIDGE=1
